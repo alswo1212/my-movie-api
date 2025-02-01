b7c571950e9e52ad51b351735cd6c6269234894f
minjae <minjagot@gmail.com>
2025-02-02 01:08:08 +0900

    test

--- src/db.js
+++ src/db.js
@@ -3,11 +3,12 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    console.log('try connect', process.env.MONGO_URI);
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log('connected', process.env.MONGO_URI);
+    console.log('connect success');
 
   } catch (err) {
     process.exit(1);
--- src/routes/reviewsRoutes.js
+++ src/routes/reviewsRoutes.js
@@ -7,41 +7,60 @@ const getReviews = async (movie_cd) => {
   reviews.forEach(rv => rv._id = rv._id.toString());
   return reviews;
 }
-
+const bad = { msg: 'ㅗㅗ' };
 router.get("/:movieCd", async (req, res) => {
   const { movieCd } = req.params;
-  const reviews = await getReviews(movieCd);
-  res.status(200).json(reviews);
+  try {
+    const reviews = await getReviews(movieCd);
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(bad);
+  }
 });
 
 router.post("/:movieCd", async (req, res) => {
   const { movieCd } = req.params;
   const { writer, content } = req.body;
-  const review = await Reviews.create({
-    writer, content,
-    movie_cd: movieCd,
-  });
-  console.log(review);
+  try {
+    const review = await Reviews.create({
+      writer, content,
+      movie_cd: movieCd,
+    });
+    console.log(review);
 
-  const reviews = await getReviews(movieCd);
-  res.status(200).json(reviews);
+    const reviews = await getReviews(movieCd);
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(bad);
+  }
 });
 
 router.put("/:movie_cd/:id", async (req, res) => {
   const { movie_cd, id } = req.params;
   const { content } = req.body;
-
-  await Reviews.updateOne({ _id: id }, { content });
-  const reviews = await getReviews(movie_cd);
-  res.status(200).json(reviews);
+  try {
+    await Reviews.updateOne({ _id: id }, { content });
+    const reviews = await getReviews(movie_cd);
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(bad);
+  }
 });
 
 router.delete("/:movie_cd/:id", async (req, res) => {
   const { movie_cd, id } = req.params;
 
-  await Reviews.deleteOne({ _id: id });
-  const reviews = await getReviews(movie_cd);
-  res.status(200).json(reviews);
+  try {
+    await Reviews.deleteOne({ _id: id });
+    const reviews = await getReviews(movie_cd);
+    res.status(200).json(reviews);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json(bad);
+  }
 });
 
 module.exports = router;
