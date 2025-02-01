cf549a605047b381aab288c67552f291cafffdbf
minjae <minjagot@gmail.com>
2025-02-02 00:43:35 +0900

    fix cors white list

--- src/server.js
+++ src/server.js
@@ -9,13 +9,12 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Middleware
 const allowedOrigins = ["https://alswo1212.github.io", "http://localhost:5173"];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
+      if (allowedOrigins.includes(origin)) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
